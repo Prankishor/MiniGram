24043a5e5f0e7108f653a6524c8bf89ecbcaf9a6
Prankishor <33565966+Prankishor@users.noreply.github.com>
2024-02-23 11:53:42 +0530

    changed base url

--- src/services/constants.js
+++ src/services/constants.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../auth';
 
 //export const BASE_URL = 'http://localhost:9090'
-export const BASE_URL = 'https://miniapis-env.eba-j3thz9pz.ca-central-1.elasticbeanstalk.com'
+export const BASE_URL = 'http://miniapis-env.eba-j3thz9pz.ca-central-1.elasticbeanstalk.com'
 
 export const myAxios = axios.create({
     baseURL: BASE_URL
