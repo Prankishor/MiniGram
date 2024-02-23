6e2724a031b827081fa81988e430ea94e3c4a9d4
Prankishor <33565966+Prankishor@users.noreply.github.com>
2024-02-23 15:28:02 +0530

    bug fix latest

--- src/services/constants.js
+++ src/services/constants.js
@@ -3,7 +3,7 @@ import { getToken } from '../auth';
 
 //export const BASE_URL = 'http://localhost:9090'
 //export const BASE_URL = 'http://miniapis-env.eba-j3thz9pz.ca-central-1.elasticbeanstalk.com'
-export const BASE_URL = 'https://bloggingbackend-production.up.railway.app/'
+export const BASE_URL = 'https://bloggingbackend-production.up.railway.app'
 
 export const myAxios = axios.create({
     baseURL: BASE_URL
